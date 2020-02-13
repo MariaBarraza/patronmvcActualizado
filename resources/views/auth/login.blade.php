a93c277050c2b7099b2301111de58c3dd5757b82
Fer <reikafer@gmail.com>
2020-02-13 16:55:16 -0700

    usuarios terminado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <b>AMLO-MAÑANERO NEW'S</b>
+    <b>LOGIN</b>
   </div>
   <!-- /.login-logo -->
   <div class="card">
--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AMLO-MAÑANERO</title>
+    <title>Noticias</title>
 </head>
 <body>
  
-    <h1>AMLO-MAÑANERO</h1>
+    <h1>Noticias</h1>
     <h3>Noticias recientes</h3>
     
     <table>
